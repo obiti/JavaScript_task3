8fb927c11c3bbad0ee07fef45e0b8cdd45b32fa6
obiti <62120211+obiti@users.noreply.github.com>
2020-04-19 23:17:45 +0100

    Add files via upload

---
+++ yu-gi-oh.js
@@ -0,0 +1,27 @@
+    let array= []
+    function divisibilitycheck(_arr) {
+
+        for (let i=1; i<=data; i++) {
+            // number is divisble by both 2&3&5
+        if (i%2==0 && i%3==0 && i%5==0) {
+            array-push("yu-gi-oh")
+            // else if number is divisible by both 2&3
+    } else if (i%2==0 && i%3==0){
+            array.push("yu-gi")
+            // else if number is divisible by both 3&5
+    } else if (i%3==0 && i%5==0) {
+            array.push("gi-oh")
+            // else if number is divisible only by 2
+    } else if (i%2==0) {
+            array.push("yu")
+            // else if number is divisible by 3
+    } else if (i%3==0) {
+            array.push("gi")
+            //else if number is divisible by 5
+    } else if (i%5==0) {
+            array.push("oh")
+    }
+        }
+     }   
+    console.log(Array)
+    integer(100)
